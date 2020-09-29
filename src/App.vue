969aeb658e5ee611c3a322c3f5bb6a55c2f00275
Hunter Dick <hunterdick@ChinaDick-2.local>
2020-09-29 14:20:32 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" msg2="Hello Papa" />
-    <HelloPapa msg="Hello Papa" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import HelloPapa from "./components/HelloPapa.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    HelloPapa,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -25,7 +14,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #0d09f9;
-  margin-top: 60px;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
